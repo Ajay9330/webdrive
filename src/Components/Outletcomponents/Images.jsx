7792bcbc646f04222ae550c7b9a06290c88117aa
Ajaykumar Yadav <ajaykumar9330c@gmail.com>
2024-09-05 01:04:07 +0530

    Added the images

--- src/Components/Outletcomponents/Images.jsx
+++ src/Components/Outletcomponents/Images.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-const Images = () => {
+const Images = ({ data }) => {
+
+
+  const d = new Date();
+  const formattedDate = `${d.getDate()}/${d.getMonth() + 1}/${d.getFullYear()}`;
+  const formattedTime = `${d.getHours()}:${d.getMinutes().toString().padStart(2, '0')}`; // Pad minutes to always show two digits
+
   return (
-    <div>Images</div>
-  )
+    <div className='w-72 grow-[2] border-opacity-10 rounded-xl border-white border-[1px] hover:border-opacity-60 cursor-pointer mx-auto items-center gap-2 p-2 justify-center flex flex-col'>
+      <article className='w-full sm:w-full justify-center text-[0.8rem] font-light gap-2 flex'>
+        <h2 className='font-normal'>Title</h2>
+        <span>{formattedDate}</span>
+        <span>{formattedTime}</span>
+      </article>
+      <div className='w-full h-full'>
+        <img 
+          className='rounded-2xl object-cover max-h-44 w-full p-2' 
+          src={data.url} 
+          alt="Descriptive text" 
+          loading="lazy"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Images
+
+
+export default Images;
--- src/pages/ImagePage.jsx
+++ src/pages/ImagePage.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Images from '../Components/Outletcomponents/Images'
-
+import classNames from 'classnames';
 export const ImagePage = () => {
+  const [imgdata,setImgData]=useState([]);
+  const fetchImgData=async()=>{
+    try {
+      // const res=await fetch('https://picsum.photos/v2/list'); 
+      const res=await fetch('https://jsonplaceholder.typicode.com/photos') 
+      const data= await res.json();
+      setImgData(data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    fetchImgData();
+  },[])  
   return (
-    <div>
-      <Images/>
+    <div className={classNames('flex flex-wrap justify-center w-full gap-2')}>
+      {imgdata.map(d=><Images data={d} />)}
       
     </div>
   )
